c0e46048a0db83b639261a9af6dcc6207ae4c612
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-02-25 21:49:21 +0300

    Fetching recipe data

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function FoodDetails({ foodId }) {
-  return <div>Food Details {foodId}</div>;
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "68546968b23e4b83adca05483f3e46df";
+  useEffect(() => {
+    async function fetchFood() {
+      try {
+        const response = await axios.get(URL, {
+          params: {
+            apiKey: API_KEY,
+          },
+        });
+        console.log(response.data);
+        setFood(response.data);
+      } catch (error) {
+        console.error("Error fetching food:", error);
+      }
+    }
+    fetchFood();
+  }, [foodId]);
+  // foodId her değiştiğinde fetch işlemi tekrar yapılacak
+  return (
+    <div>
+      Food Details {foodId}
+      {food.title}
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
